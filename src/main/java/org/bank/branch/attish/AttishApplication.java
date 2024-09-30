6b82749c1c94b656d94bdcde797a7af534220f89
school <muhammad.shamaeem@elev.ga.ntig.se>
2024-09-30 09:47:45 +0200

    create boilerplate

---
+++ src/main/java/org/bank/branch/attish/AttishApplication.java
@@ -0,0 +1,13 @@
+package org.bank.branch.attish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttishApplication.class, args);
+	}
+
+}
