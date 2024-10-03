7b3860f0f41c59cd1301ea693cd6490fbf4a4db4
attish <muhammadshamaeem@gmail.com>
2024-10-03 18:37:54 +0200

    create token service

--- src/main/java/org/bank/branch/attish/AttishApplication.java
+++ src/main/java/org/bank/branch/attish/AttishApplication.java
@@ -1,9 +1,15 @@
 package org.bank.branch.attish;
 
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class AttishApplication {
 
 	public static void main(String[] args) {
